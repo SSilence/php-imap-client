9bd98e14eb39f0d425fca80d334aed3dd1513db6
sergey144010 <sergey144010@mail.ru>
2016-06-26 02:57:17 +0800

    Add two examples how use it.

---
+++ example-composer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace program;
+
+require_once "vendor/autoload.php";
+
+use SSilence\ImapClientException;
+use SSilence\Imap;
+
+$mailbox = 'my.imapserver.com';
+$username = 'username';
+$password = 'secret';
+$encryption = 'tls'; // or ssl or ''
+
+// open connection
+try{
+    $imap = new Imap($mailbox, $username, $password, $encryption);
+}catch (ImapClientException $error){
+    echo $error->getMessage().PHP_EOL;
+    die();
+}
+
+// get all folders as array of strings
+$folders = $imap->getFolders();
+foreach($folders as $folder)
+    echo $folder;
+
+// select folder Inbox
+$imap->selectFolder('INBOX');
+
+// count messages in current folder
+$overallMessages = $imap->countMessages();
+$unreadMessages = $imap->countUnreadMessages();
+
+// fetch all messages in the current folder
+$emails = $imap->getMessages();
+var_dump($emails);
+
+// add new folder for archive
+$imap->addFolder('archive');
+
+// move the first email to archive
+$imap->moveMessage($emails[0]['uid'], 'archive');
+
+// delete second message
+$imap->deleteMessage($emails[1]['uid']);
