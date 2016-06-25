84124e7472717e258b2763cd543423a3b51bf9e9
sergey144010 <sergey144010@mail.ru>
2016-06-26 01:34:35 +0800

    If error in imap server add exception

--- ImapClient/Imap.php
+++ ImapClient/Imap.php
@@ -42,7 +42,6 @@ class Imap {
      * @param $encryption ssl or tls
      */
     public function __construct($mailbox, $username, $password, $encryption = false) {
-        # php.ini ;extension=php_imap.dll
         if (!function_exists('imap_open')) {
             throw new ImapClientException('Imap function not available');
         };
@@ -52,7 +51,10 @@ class Imap {
         else if($encryption!=null && isset($encryption) && $encryption=='tls')
             $enc = '/imap/tls/novalidate-cert';
         $this->mailbox = "{" . $mailbox . $enc . "}";
-        $this->imap = imap_open($this->mailbox, $username, $password);
+        $this->imap = @imap_open($this->mailbox, $username, $password);
+        if ($this->imap === false) {
+            throw new ImapClientException('Not connect with '.$mailbox);
+        };
     }
     
     /**
