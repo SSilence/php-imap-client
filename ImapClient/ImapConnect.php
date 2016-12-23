508be959c3fdad2a0337550173319aedca3992c5
xXXIMMATTHEWXXx <mattparksjr@gmail.com>
2016-12-22 17:53:01 -0800

    Comments

--- ImapClient/ImapConnect.php
+++ ImapClient/ImapConnect.php
@@ -49,6 +49,19 @@ use SSilence\ImapClient\ImapClientException;
  * $mailbox = $connect->getResponseMailbox();
  *
  * To view the current mailbox string you can use everywhere getMailbox() method.
+ *
+ * Copyright (C) 2016  SSilence
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @package    protocols
+ * @copyright  Copyright (c) Tobias Zeising (http://www.aditu.de)
+ * @author     Tobias Zeising <tobias.zeising@aditu.de>
  */
 class ImapConnect
 {
