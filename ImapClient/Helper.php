46dfcb46232558e6f315f063c65f430e1951c4ca
sergey144010 <sergey144010@mail.ru>
2017-03-26 18:00:51 +0800

    Add Helpers

---
+++ ImapClient/Helper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Мария
+ * Date: 26.03.2017
+ * Time: 17:21
+ */
+
+namespace SSilence\ImapClient;
+
+
+class Helper
+{
+
+}
---
+++ ImapClient/OutgoingMessage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Мария
+ * Date: 26.03.2017
+ * Time: 16:59
+ */
+
+namespace SSilence\ImapClient;
+
+
+class OutgoingMessage
+{
+
+}
