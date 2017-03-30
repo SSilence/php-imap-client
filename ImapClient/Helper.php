02c3c494b5e89fb2288a2e14d3bf4dfc4a4945e8
mattparksjr <mattparksjr@gmail.com>
2017-03-30 07:46:10 -0400

    Added proper copy-right notice

--- ImapClient/Helper.php
+++ ImapClient/Helper.php
@@ -2,6 +2,22 @@
 
 namespace SSilence\ImapClient;
 
+/**
+ * Helper class the internals of php-imap-client
+ *
+ * Copyright (C) 2016-2017  SSilence
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @package    protocols
+ * @copyright  Copyright (c) Tobias Zeising (http://www.aditu.de)
+ * @author     Tobias Zeising <tobias.zeising@aditu.de>
+ */
 
 class Helper
 {
