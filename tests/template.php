b8f67d864ec8ab92f6aa368cec3e24ad73606c9f
sergey144010 <sergey144010@mail.ru>
2017-04-04 20:49:22 +0800

    Add template for testings

--- tests/template.php
+++ tests/template.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Мария
- * Date: 04.04.2017
- * Time: 20:47
- */
+
+namespace program;
+
+require_once "../ImapClient/ImapClientException.php";
+require_once "../ImapClient/ImapConnect.php";
+require_once "../ImapClient/ImapClient.php";
+require_once "../ImapClient/IncomingMessage.php";
+require_once "../ImapClient/TypeAttachments.php";
+require_once "../ImapClient/TypeBody.php";
+require_once "../ImapClient/OutgoingMessage.php";
+require_once "../ImapClient/Helper.php";
+
+use SSilence\ImapClient\Helper;
+use SSilence\ImapClient\ImapClientException;
+use SSilence\ImapClient\ImapConnect;
+use SSilence\ImapClient\ImapClient;
+use SSilence\ImapClient\OutgoingMessage;
+
+try{
+
+    $imap = new ImapClient([
+        'flags' => [
+            'service' => ImapConnect::SERVICE_IMAP,
+            'encrypt' => ImapConnect::ENCRYPT_SSL,
+            'validateCertificates' => ImapConnect::NOVALIDATE_CERT,
+        ],
+        'mailbox' => [
+            'remote_system_name' => '',
+            'mailbox_name' => '',
+        ],
+        'connect' => [
+            'username' => '',
+            'password' => ''
+        ]
+    ]);
+
+
+
+}catch (ImapClientException $error){
+    echo $error->getInfo();
+};
