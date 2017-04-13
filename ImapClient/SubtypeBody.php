b07630032b1c2e2f0069893e0e63662334490250
sergey144010 <sergey144010@mail.ru>
2017-04-13 18:19:32 +0800

    Add SubtypeBody

--- ImapClient/SubtypeBody.php
+++ ImapClient/SubtypeBody.php
@@ -1,15 +1,16 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Мария
+ * User: Sergey144010
  * Date: 13.04.2017
  * Time: 17:11
  */
 
 namespace SSilence\ImapClient;
 
+use SSilence\ImapClient\Section;
 
-class SubtypeBody
+class SubtypeBody extends Section
 {
-
+    public $charset;
 }
