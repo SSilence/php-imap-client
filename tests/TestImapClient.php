7a36a15bc938ba347f6f8a597403a39762703c9f
sergey144010 <sergey144010@mail.ru>
2017-04-24 21:56:12 +0800

    Extend Imap Client

---
+++ tests/TestImapClient.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Мария
+ * Date: 24.04.2017
+ * Time: 20:54
+ */
+
+namespace tests;
+
+
+class TestImapClient
+{
+
+}
---
+++ tests/tests.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Мария
+ * Date: 24.04.2017
+ * Time: 20:55
+ */
