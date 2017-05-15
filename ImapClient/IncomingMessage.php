a628387d4ad815efa971603d6bafae4321860315
Matthew Parks <mattparksjr@gmail.com>
2017-05-15 07:40:32 -0400

    Revert travisIsABaby
    Maybe I messed it up

--- ImapClient/IncomingMessage.php
+++ ImapClient/IncomingMessage.php
@@ -359,8 +359,7 @@ class IncomingMessage
         $objNew = new \stdClass();
         $i = 1;
         foreach ($this->getSections(self::SECTION_BODY) as $section) {
-			$travisIsABaby = array('class' => SubtypeBody:class);
-            $obj = $this->getSection($section, $travisIsABaby);
+            $obj = $this->getSection($section, array('class' => SubtypeBody::class));
             $subtype = strtolower($obj->__get('structure')->subtype);
             if (!isset($objNew->$subtype)) {
                 $objNew->$subtype = $obj;
